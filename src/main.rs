90b0c986b4c2f58dc100c12f5986811dadbeed93
The-Fool-cmd <calin.sandu.94@gmail.com>
2026-02-09 17:59:39 +0200

    Should be final.

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,19 @@ use ratatui::{
     crossterm::event::{self, Event},
     layout::{Constraint, Layout},
     style::{Color, Style, Stylize},
+    symbols::Marker,
     text::Line,
-    widgets::{Axis, Block, Chart, Paragraph},
+    widgets::{Axis, Block, Chart, Dataset, Paragraph},
 };
 use std::time::{Duration, Instant};
 
 // Stores state of app
 struct App {
-    freq: f32,
-    amp: f32,
+    freq: f64,
+    amp: f64,
     started_at: Instant,
     should_quit: bool,
+    plot: Vec<(f64, f64)>,
 }
 
 // bougee
@@ -25,6 +27,7 @@ impl Default for App {
             amp: 1.0,
             started_at: Instant::now(),
             should_quit: false,
+            plot: Vec::new(),
         }
     }
 }
@@ -36,13 +39,26 @@ impl App {
             KeyCode::Char('q') => {
                 self.should_quit = true;
             }
-            KeyCode::Left => self.freq = (self.freq - 0.1).max(1.0),
-            KeyCode::Right => self.freq = (self.freq + 0.1).min(10.0),
-            KeyCode::Down => self.amp = (self.amp - 0.1).max(0.0),
-            KeyCode::Up => self.amp = (self.amp + 0.1).min(10.0),
+            KeyCode::Left => {
+                self.freq = (self.freq - 0.1).max(1.0);
+                self.update_plot();
+            }
+            KeyCode::Right => {
+                self.freq = (self.freq + 0.1).min(10.0);
+                self.update_plot();
+            }
+            KeyCode::Down => {
+                self.amp = (self.amp - 0.1).max(0.0);
+                self.update_plot();
+            }
+            KeyCode::Up => {
+                self.amp = (self.amp + 0.1).min(10.0);
+                self.update_plot();
+            }
             KeyCode::Char('r') => {
                 self.amp = 1.0;
                 self.freq = 1.0;
+                self.update_plot();
             }
             _ => {}
         }
@@ -64,17 +80,26 @@ impl App {
         // Create debug paragraph from 'debug_text'
         let debug_line: Paragraph = Paragraph::new(debug_text)
             .block(Block::bordered().title("Debug Line".bold().into_centered_line()));
-        // Create chart from... nothing yet
-        let graph: Chart = Chart::default()
+
+        // Create dataset using the vector of coordinates
+        let dataset = Dataset::default()
+            .marker(Marker::Braille)
+            .style(Style::default().fg(Color::Cyan))
+            .data(&self.plot);
+
+        // Create chart from with the dataset
+        let graph: Chart = Chart::new(vec![dataset])
             .x_axis(
                 Axis::default()
                     .title("X Axis")
-                    .style(Style::default().fg(Color::LightBlue)),
+                    .style(Style::default().fg(Color::LightBlue))
+                    .bounds([-10.0, 10.0]),
             )
             .y_axis(
                 Axis::default()
                     .title("Y Axis")
-                    .style(Style::default().fg(Color::LightRed)),
+                    .style(Style::default().fg(Color::LightRed))
+                    .bounds([-10.0, 10.0]),
             )
             .block(Block::bordered().title("Sine Wave".bold().into_centered_line()));
 
@@ -82,12 +107,29 @@ impl App {
         frame.render_widget(debug_line, top);
         frame.render_widget(graph, bottom);
     }
+
+    fn update_plot(&mut self) {
+        self.plot.clear();
+        let n = 10000;
+        let x_min = -10.0;
+        let x_max = 10.0;
+        let span = x_max - x_min;
+
+        for i in 0..n {
+            let t = i as f64 / (n - 1) as f64;
+            let x = x_min + t * span;
+            let y = self.amp * (self.freq * x).sin();
+            self.plot.push((x, y));
+        }
+    }
 }
 
 fn main() -> Result<(), std::io::Error> {
     let mut terminal = ratatui::init();
 
     let mut app = App::default();
+    app.update_plot();
+
     loop {
         if app.should_quit {
             break;
